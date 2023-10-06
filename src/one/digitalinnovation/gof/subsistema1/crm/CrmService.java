995d766b5bbfdb84986aa150846281b685f176c3
Vinicius <vinicius@iconax.com.br>
2023-10-06 16:46:27 -0300

    learning facade

---
+++ src/one/digitalinnovation/gof/subsistema1/crm/CrmService.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.gof.subsistema1.crm;
+
+public class CrmService {
+	
+	public static void gravarCliente(String nome, String cep, String cidade) {
+		System.out.println("Cliente salvo!");
+	}
+	
+	public CrmService() {
+		super();
+	}
+
+}
